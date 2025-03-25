8cfdb629c3f8cda4d801f3f5bfba83f55cb79b68
“Aaron <amineen64@hotmail.com>
2025-03-24 21:55:04 -0500

    refactor: redefine VendorSalesSummary and introduce Payment and Amount types

--- models/models.go
+++ models/models.go
@@ -28,12 +28,21 @@ type MonitoryValue struct {
 	Currency string `bson:"currency,omitempty"`
 }
 
-type VendorSalesSummary struct {
-	ID                primitive.ObjectID `bson:"_id,omitempty"`
-	Date              string             `bson:"date,omitempty"`
-	VendingStation    string             `bson:"vendingStation,omitempty"`
-	TotalAmountSold   float32            `bson:"totalAmountSold,omitempty"`
-	TotalKwhSold      float32            `bson:"totalKwhSold,omitempty"`
-	TotalTransactions int                `bson:"totalTransactions,omitempty"`
-	UserId            string             `bson:"userId,omitempty"`
+type Payment struct {
+	ID            string             `bson:"id,omitempty"`
+	TimeStamp     time.Time          `bson:"timestamp,omitempty"`
+	RecipientId   string             `bson:"recipient_id,omitempty"`
+	CustomerId    string             `bson:"customer_id,omitempty"`
+	Amount        Amount             `bson:"amount,omitempty"`
+	Memo          string             `bson:"memo,omitempty"`
+	ExternalId    string             `bson:"external_id,omitempty"`
+	ServiceAreaID primitive.ObjectID `bson:"service_area_id,omitempty"`
+	Vendor        string             `bson:"vendor,omitempty"`
+	VendorId      string             `bson:"vendorId,omitempty"`
+}
+
+type Amount struct {
+	Value  string  `bson:"value,omitempty"`
+	Amount string  `bson:"amount,omitempty"`
+	Kwh    float32 `bson:"kwh,omitempty"`
 }
